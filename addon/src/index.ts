533fa5e8197bfa57968031ec1da111ad820cc22d
hlchanad <hlchanad@stu.ust.hk>
2025-02-02 22:53:41 +0900

    Create a fastify server

--- addon/src/index.ts
+++ addon/src/index.ts
@@ -1,3 +1,17 @@
-export const target: string = 'World';
+import Fastify from 'fastify';
 
-console.log("Hello " + target);
+const fastify = Fastify({
+  logger: true
+});
+
+fastify.get('/electricity-usages', async (req, res) => {
+  fastify.log.info('Retrieving electricity usages');
+  return [{ foo: 'bar'} ];
+});
+
+const port = 3000;
+fastify.listen({ host: '0.0.0.0', port })
+  .catch(err => {
+    fastify.log.error(err);
+    process.exit(1);
+  });
