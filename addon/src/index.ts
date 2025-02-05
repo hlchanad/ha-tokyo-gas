e66bc5febac8833298548db808d5a5fd5225fc65
hlchanad <hlchanad@stu.ust.hk>
2025-02-05 10:24:15 +0900

    Add schema validation

--- addon/src/index.ts
+++ addon/src/index.ts
@@ -4,10 +4,29 @@ const fastify = Fastify({
   logger: true
 });
 
-fastify.get('/electricity-usages', async (req, res) => {
-  fastify.log.info('Retrieving electricity usages');
-  return [{ foo: 'bar'} ];
-});
+fastify.get(
+  '/electricity-usages',
+  {
+    schema: {
+      querystring: {
+        type: 'object',
+        properties: {
+          username: { type: 'string' },
+          password: { type: 'string' },
+          customerNumber: { type: 'string' },
+        },
+        required: ['username', 'password', 'customerNumber'],
+      },
+    },
+  },
+  async (req, res) => {
+    fastify.log.info('Retrieving electricity usages');
+
+    fastify.log.info(`query: ${JSON.stringify(req.query)}`);
+
+    return [{ foo: 'bar'} ];
+  },
+);
 
 const port = 3000;
 fastify.listen({ host: '0.0.0.0', port })
